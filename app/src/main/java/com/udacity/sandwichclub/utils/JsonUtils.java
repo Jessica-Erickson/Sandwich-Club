599ea1316daecc26c000bcfc39b830ebb7760af2
Intrepid Panda <jerick999@gmail.com>
2019-02-26 07:22:01 -0700

    Get data fully parsed

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,11 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class JsonUtils {
@@ -13,11 +15,19 @@ public class JsonUtils {
         JSONObject data= new JSONObject(json);
         JSONObject name = data.getJSONObject("name");
         String mainName = name.getString("mainName");
-        List<String> alsoKnownAs = name.getJSONArray("alsoKnownAs");
+        List<String> alsoKnownAs = new ArrayList<String>();
+        JSONArray akas = name.getJSONArray("alsoKnownAs");
+        for (int i=0;i<akas.length();i++) {
+            alsoKnownAs.add(akas.getString(i));
+        }
         String placeOfOrigin = data.getString("placeOfOrigin");
         String description = data.getString("description");
         String image = data.getString("image");
-        List<String> ingredients = data.getJSONArray("ingredients");
+        List<String> ingredients = new ArrayList<String>();
+        JSONArray ingr = data.getJSONArray("ingredients");
+        for (int j=0;j<ingr.length();j++) {
+            ingredients.add(ingr.getString(j));
+        }
         Sandwich sandwich = new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
         return sandwich;
     }
