d299e9a332141eb9a70d23a03a7c2194a17472e1
Intrepid Panda <jerick999@gmail.com>
2019-02-25 22:38:41 -0700

    Set up parseSandwichJson to parse JSON string and return a sandwich instance

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,23 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+        JSONObject data= new JSONObject(json);
+        JSONObject name = data.getJSONObject("name");
+        String mainName = name.getString("mainName");
+        List<String> alsoKnownAs = name.getJSONArray("alsoKnownAs");
+        String placeOfOrigin = data.getString("placeOfOrigin");
+        String description = data.getString("description");
+        String image = data.getString("image");
+        List<String> ingredients = data.getJSONArray("ingredients");
+        Sandwich sandwich = new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+        return sandwich;
     }
 }
